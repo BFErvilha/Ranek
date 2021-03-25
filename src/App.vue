d9354a05fd4085a80cf451601c4974b5a8e93799
Bruno Ervilha <bruno.ervilha@shiftinc.com.br>
2021-03-25 18:24:06 -0300

    feat: Header ande Footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,104 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view/>
+    <TheHeader />
+    <main id="main">
+      <router-view/>
+    </main>
+    <TheFooter />
   </div>
 </template>
+<script>
+import TheHeader from "@/components/TheHeader.vue"
+import TheFooter from "@/components/TheFooter.vue"
+
+export default {
+  components:{
+    TheHeader,
+    TheFooter,
+  }
+  
+}
+</script>
 
 <style lang="scss">
 
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
+
+*{
+  box-sizing: border-box;
+}
+
+body, ul , li, h1,h2,p{
+  padding: 0px;
+}
+ul{
+  list-style: none;
+}
+
+body{
+  font-family: 'Nunito';
+  color: #345;
+  background: url('./assets/pattern.svg') repeat top;
+}
+
+a{
+  color: #345;
+  text-decoration: none;
+}
+
+img{
+  max-width: 100%;
+  display:block;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 7px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background:  #87F; 
+  border-radius: 30px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #65d; 
+}
+
+.btn{
+  display: block;
+  padding: 10px 30px;
+  background: #87F;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30,60,90,0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: 'Nunito';
+  cursor: pointer;
+
+  &:hover{
+    background: #65d;
+    transform: scale(1.1);
+  }
+}
+
+#app{
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+
+  #main{
+    flex: 1;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-   
-  </div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProdutosBuscar/>
+    <ProdutosListar/>
+  </section>
 </template>
 
 <script>
-
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue"
+import ProdutosListar from "@/components/ProdutosListar.vue"
 
 export default {
   name: 'Home',
+  components:{
+    ProdutosBuscar, 
+    ProdutosListar
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  h1{
+    font-size: 2rem;
+    color: #87f;
+    margin-top: 40px;
+    text-align: center;
+  }
+</style>
